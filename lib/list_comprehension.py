854fc8a35d3f28dc1da08f63cd6173a3271fcca1
MutaiNewton56 <mutainewton56.gmail.com>
2024-06-06 11:44:59 +0300

    comprehensions and generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(int_list):
+    return [x for x in int_list if x % 2 == 0]
 
-def make_exclamation(sentence_list):
-    pass
+# Example usage:
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+evens = return_evens(numbers)
+
+
+
+def make_exclamation(sentences):
+    return [sentence + '!' for sentence in sentences]
+
+sentences = ["Hello", "How are you", "Good morning", "Goodbye"]
+exclamations = make_exclamation(sentences)
+
